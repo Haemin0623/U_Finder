9f779e90ffb4ca8edcf6b575f519df7a09d8aeb5
SUNNY\1seon <1seonheemail@gmail.com>
2022-09-15 15:51:17 +0900

    sql 작업 완료

--- src/main/webapp/sql.sql
+++ src/main/webapp/sql.sql
@@ -1,60 +1,76 @@
-create table MOVIE (
-	MOVIENO		number(4) primary key not null,
-	MOVIENAME	varchar2(50) not null,
-	STORY		varchar2(1000),
-	DIRECTOR	varchar2(20),
-	PLAYTIME	number(4),
-	NETFLIX		varchar2(1) default 'N' not null,
-	DISNEY		varchar2(1) default 'N' not null,
-	COUPANG		varchar2(1) default 'N' not null,
-	TVING		varchar2(1) default 'N' not null,
-	GENRE		varchar2(20),
-	CNT			number(4) default 0 not null,
-	POSTER		varchar2(50),
-	TRAILER		varchar2(500)
+-- 영화
+drop table movie;
+create table movie (
+    movieno number(4) primary key not null,
+    moviename varchar2(50) not null,
+    story varchar2(1000),
+    director varchar2(20),
+    playtime number(4),
+    netflix varchar2(1) default 'N' not null,
+    disney varchar2(1) default 'N' not null,
+    coupang varchar2(1) default 'N' not null,
+    tving varchar2(1) default 'N' not null,
+    genre varchar2(20),
+    cnt number(4) default 0 not null,
+    poster varchar2(50),
+    trailer varchar2(500)
+);
+-- 회원
+create table member (
+    memberno number(4) primary key not null,
+    email varchar2(30) not null,
+    password varchar2(30) not null,
+    nickname varchar2(30) not null,
+    netflix varchar2(1) default 'N' not null,
+    disney varchar2(1) default 'N' not null,
+    coupang varchar2(1) default 'N' not null,
+    tving varchar2(1) default 'N' not null,
+    memberout varchar2(1) default 'N' not null
 );
 
-create table MEMBER (
-	MEMBERNO	number(4) not null primary key,
-	EMAIL		varchar2(30) not null,
-	PASSWORD	varchar2(30) not null,
-	NICKNAME	varchar2(30) not null,
-	NETFLIX     varchar2(1) default 'N' not null,
-	DISNEY      varchar2(1) default 'N' not null, 
-	COUPANG     varchar2(1) default 'N' not null,
-	TVING       varchar2(1) default 'N' not null,
-	MEMBEROUT   varchar2(1) default 'N' not null 
+-- 태그 
+drop table tag;
+create table tag (
+    tagno number(4) primary key not null,
+    tag varchar2(20) not null,
+    movieno number(4) not null,
+    constraint fk_tag foreign key(movieno) references movie(movieno)
+);
+-- 캘린더
+create table calendar (
+    calno number(4) primary key not null,
+    movieno number(4) not null,
+    opendate date not null,
+    constraint fk_calendar foreign key(movieno) references movie(movieno)
+);
+-- 찜
+drop table pick;
+create table pick (
+    pickno number(4) primary key not null,
+    movieno number(4) not null,
+    memberno number(4) not null,
+    constraint fk_pick1 foreign key(movieno) references movie(movieno),
+    constraint fk_pick2 foreign key(memberno) references member(memberno)
 );
 
-create table MOVIE 
-TEST 해민
-TEST 해민2
-test ㅅ인
-create table MOVIE 
-TEST 해민
-TEST 해민2
-test ㅅ인
-create table MOVIE 
-TEST 해민
-TEST 해민2
-test ㅅ인
-create table MOVIE 
-TEST 해민
-TEST 해민2
-test ㅅ인
-create table MOVIE 
-TEST 해민
-TEST 해민2
-test ㅅ인
-create table MOVIE 
-TEST 해민
-TEST 해민2
-test ㅅ인
-create table MOVIE 
-TEST 해민
-TEST 해민2
-test ㅅ인
-create table MOVIE 
-TEST 해민
-TEST 해민2
-test ㅅ인
+-- 리뷰
+create table review (
+    reviewno number(4) primary key not null,
+    movieno number(4) not null,
+    memberno number(4) not null,
+    content varchar2(200) not null,
+    movielike number(1) default 0 not null,
+    del varchar2(1) default 'N' not null,
+    constraint fk_review1 foreign key(movieno) references movie(movieno),
+    constraint fk_review2 foreign key(memberno) references member(memberno)
+);
+
+-- 배우
+create table actor (
+    actorno number(4) primary key not null,
+    actorname varchar2(30) not null,
+    movieno number(4) not null,
+    constraint fk_actor foreign key(movieno) references movie(movieno)
+);
+
+
