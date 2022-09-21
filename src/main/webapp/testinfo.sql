5ef3419477983f02f037bec627c7d7780e2e91c3
이해민 <haemin0623@gmail.com>
2022-09-21 19:42:12 +0900

    충돌수정

--- src/main/webapp/testinfo.sql
+++ src/main/webapp/testinfo.sql
@@ -210,10 +210,10 @@ select count(*) from member;
 
 select moviename from movie where movieno in(select movieno from pick where memberno in(select memberno from member where id = #{id));
 
-<<<<<<< HEAD
+
 select *from member where id = 'test@test.com';
-=======
+
 select mm.*, pp.*  from (select * from (select rowNum rn, a.* from (select * from movie order by movieno) a) where rn between 1 and 10) mm, (select count(*) from pick, movie where pick.movieno=movie.movieno ) pp;
 
 select m.*, count(p.memberno) from pick p, movie m where p.movieno=m.movieno;
->>>>>>> 3b21a8c (no message)
+
