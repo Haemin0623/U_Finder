5672b7a092cb84cd166b804a4b6ab19d58eb9ee2
Sooin Park <sooin818@gmail.com>
2022-09-30 15:55:50 +0900

    수인_sql

---
+++ src/main/webapp/info_soo.sql
@@ -0,0 +1,73 @@
+
+insert into movie values (60, '헝거게임: 판엠의 불꽃', '세상을 변화시킬 거대한 혁명의 불꽃이 타오른다! 12개의 구역으로 이루어진 독재국가 ‘판엠’이 체재를 유지하기 위해 만든 생존 전쟁 ‘헝거게임’. 일년에 한번 각 구>역에서 추첨을 통해 두 명을 선발, 총 24명이 생존을 겨루게 되는 것. ‘헝거게임’의 추첨식에서 ‘캣니스’는 어린 여동생의 이름이 호명되자 동생을 대신해 참가를 자청하며 주목을 받는다. 과거 자신을 위기에서 구해줬던 ‘피타’ 역시 선발되어 미묘한 감정에 휩싸인다. ‘캣니스’는 금지구역에서 함께 사냥을 했던 ‘게일’에게 가족을 부탁하며 생존을 겨루게 될 판엠의 수도 ‘캐피톨’로 향하는데…', '게리 로스', 142, 'Y', 'Y', 'N', 'N', '판타지', 0, '60TheHungerGames.jpg', 'https://www.youtube.com/embed/IQFWOHEgHCc');
+insert into tag values (301, '액션', 60);
+insert into tag values (302, '소설', 60);
+insert into actor values (301, '제니퍼 로렌스', 60);
+insert into actor values (302, '리암 헴스워스', 60);
+
+insert into movie values (61, '헝거 게임: 캣칭 파이어 ', '헝거게임의 우승으로 독재국가 ‘판엠’의 절대권력을 위협하는 존재가 된 캣니스, 혁명의 불꽃이 된 그녀를 제거하기 위해 캐피톨은 75회 스페셜 헝거게임의 재출전을 강요한다. 역대 최강의 우승자들이 모인 헝거게임에 참가하게 된 캣니스는 판엠의 음모 속에서 적인지 동료인지 알 수 없는 막강한 도전자들과 맞닥뜨린다. 모두의 운명을 걸고 살아남아야 하는 캣니스, 그녀와 함께 혁명의 불꽃이 시작된다.', '프랜시스 로렌스', 130, 'N', 'Y', 'Y', 'N', '판타지', 0, '61TheHungerGamesCatchingFire.jpeg', 'https://www.youtube.com/embed/hYOpyeN4C7o');
+insert into tag values (303, '액션', 61);
+insert into tag values (304, '소설', 61);
+insert into actor values (303, '제니퍼 로렌스', 61);
+insert into actor values (304, '조시 허처슨', 61);
+
+
+insert into movie values (62, '헝거 게임: 모킹제이', '헝거게임이 끝나고 캣니스의 고향인 12구역이 캐피톨의 폭격으로 파괴되자 그녀는 절망감으로 괴로워한다. 12구역의 생존자들이 13구역에 있다는 것을 알게 된 캣니스는 반정부 세력의 중심이 된다. 어떤 공격도, 어떤 감시도 피할 수 있는 전설의 13구역을 다스리는 대통령 코인은 캣니스에게 혁명의 불꽃이자 반군의 상징인 모킹제이가 되어달라고 부탁하고, 모두의 희망이 된 캣니스는 이제 세상을 구할 반격을 시작한다.', '프랜시스 로렌스', 143, 'Y', 'Y', 'Y', 'N', '판타지', 0, '62TheHungerGamesMockingjayPart1.jpeg', 'www.youtube.com/embed/kUdNL-NJylk');
+insert into tag values (305, '액션', 62);
+insert into tag values (306, '소설', 62);
+insert into actor values (305, '제니퍼 로렌스', 62);
+insert into actor values (306, '줄리안 무어', 62);
+
+insert into movie values (63, '헝거게임: 더 파이널', '두 번의 헝거게임에서 살아남은 생존자 캣니스 에버딘. 그런 그녀를 향한 스노우 대통령의 공격은 더욱 거세지고 캣니스의 분노는 폭발한다.캣니스는 그를 멈추고 모든 것을 끝낼 수 있는 사람이 바로 자신임을 깨닫고, 13구역 군단에 합류한다. 철저하게 준비를 끝낸 캣니스와 최정예 요원들은 캐피톨 입성에 성공하지만 스노우 대통령이 설치한 함정들로 인해 목숨을 위협받는 위험한 상황에 처하게 된다. 희생된 동료들을 뒤로한 채 가까스로 스노우 대통령과의 대결을 앞둔 캣니스에게 또 다른 충격적인 사건이 벌어지는데… 마침내 최후의 승자가 결정된다!', '프랜시스 로렌스', 124, 'N', 'Y', 'N', 'N', '판타지', 3500, '63TheHungerGamesMockingjayPart2.jpeg', 'www.youtube.com/embed/22L8ANXQxc4');
+insert into tag values (307, '액션', 63);
+insert into tag values (308, '소설', 63);
+insert into actor values (307, '제니퍼 로렌스', 63);
+insert into actor values (308, '줄리안 무어', 63);
+
+
+insert into movie values (64, '혹성탈출: 진화의 시작', '치매 치료제로 개발중인 약물 ALZ-112을 주사받은 실험용 침팬지 밝은 눈에게서 태어났기 때문에 선천적으로 높은 지능과 밝은 녹색의 눈동자를 가지고 있었다.밝은 눈이 시저를 지키려는 보호본능 때문에 난동을 부리다가 죽자, ALZ-112가 실패했다고 단정한 GEN-SYS사의 CEO 스티븐 제이컵스가 ALZ-112을 실험받은 모든 유인원을 죽이라는 명령을 내렸기 때문에 시저도 죽어야 했지만, 연구원 로버트 프랭클린은 아기인 시저를 차마 죽이지 못하고 윌 로드먼에게 시저를 어찌할 것인지를 맡긴다. 윌 역시 차마 안락사를 할 수 없었기 때문에 시저를 집으로 데려온다. 시저가 빠른 속도로 수화를 익히는 등 놀라운 지능을 보이고 치매에 걸린 윌의 아버지 찰스 로드먼도 시저를 좋아해서 결국 정이 들어버린 윌은 시저를 계속 기르게 된다. 시저란 이름도 윌의 아버지가 지어준 것.', '루퍼트 와이엇', 156, 'Y', 'Y', 'N', 'Y', 'SF', 0, '64DawnofthePlanetoftheApes.jpeg', 'www.youtube.com/embed/9awxx4sjIPo');
+insert into tag values (309, '어드벤처', 64);
+insert into tag values (310, '원숭이', 64);
+insert into tag values (311, '액션', 64);
+insert into actor values (309, '앤디 서키스', 64);
+insert into actor values (310, '제임스 프랭코', 64);
+insert into actor values (311, '프리다 핀토', 64);
+
+
+insert into movie values (65, '혹성탈출: 반격의 서막', '전작에서 시저(앤디 서키스 분)가 유인원 무리를 이끌고 금문교에서 인간들과 전투를 벌여 승리하고 10년이라는 시간이 흘렀다. 영화 초반부 인류가 유인원 독감에 의해 대부분의 인구가 사망하고 문명이 붕괴되고 있는 장면을 보여준다. 이 부분에서 버락 오바마(!!!)가 깨알출연한다. 감염자 수를 나타내는 빨간점들이 점점 사라지면서 "이것이 인류의 종말일지도 모릅니다. 살아남은 사람끼리 서로 싸우다가 죽어가겠지요.."라는 끝대사가 나온다. 그리고 음산한 분위기의 음악과 붉은색의 타이틀이 나오면서 영화는 시작한다.', '맷 리브스', 134, 'Y', 'Y', 'Y', 'Y', 'SF', 0, '65RiseofthePlanetoftheApes.jpeg', 'www.youtube.com/embed/W_t1XKjHd_Q');
+insert into tag values (312, '어드벤처', 65);
+insert into tag values (313, '전쟁', 65);
+insert into actor values (312, '앤디 서키스', 65);
+insert into actor values (313, '게리 올드만', 65);
+
+
+insert into movie values (66, '혹성탈출: 종의 전쟁', '15년 전 바이러스의 창궐과 2년 전 전쟁의 불씨가 되었던 코바를 죽였다는 전편들의 핵심을 요약해주는 자막[11]으로 시작된다. 인간의 군대[12][13]는 유인원의 본부를 공격하고 시저를 죽이러 잠입하며, 놀랍게도 이들 군대에는 일부 유인원들이 동참하고 있었다. 시저 편 유인원들이 참호를 만들고 방어중인 것을[14] 본 군인들이 무전으로 대령에게 보고하자 대령은 공격 명령을 내리고, 군인들은 곧 유탄발사기로 참호를 공격하고 총을 쏘며 돌격해 들어간다. 처음에는 거의 일방적으로 몰아붙이며 유인원들을 죽여나갔고 일부 유인원들이 총이나 창으로 저항하지만 전혀 피해를 입지 않았다. 그러나 살아남은 원숭이들이 곧 후방에서 유인원 기병대를 데리고 오고, 몰려온 유인원들은 연막을 뿌리고 대량의 화살과 창 공격을 가해 순식간에 군인들은 전멸하고 만다. ', '맷 리브스', 134, 'Y', 'N', 'Y', 'N', 'SF', 2000, '66WarforthePlanetoftheApes.jpeg', 'www.youtube.com/embed/w2EwkleWbl4');
+insert into tag values (314, '어드벤처', 66);
+insert into tag values (315, '전쟁', 66);
+insert into tag values (316, '액션', 66);
+insert into actor values (314, '앤디 서키스', 66);
+insert into actor values (315, '우디 해럴슨', 66);
+insert into actor values (316, '게리 올드만', 66);
+
+
+insert into movie values (67, '반지의 제왕: 반지 원정대 ', '호빗들은 고향인 샤이어로 돌아가고 샘은 로지 코튼과 결혼한다. 그로부터 몇 년 뒤, 프로도는 삼촌 빌보, 간달프, 엘프들과 함께 중간계를 떠나 영생의 땅으로 향한다. 그러면서 샘에게 두 사람의 여정을 세세히 기록한 웨스트마치의 붉은 책을 남긴다. 샤이어로 돌아온 샘이 로지와 아이들을 껴안으면서 영화는 막을 내린다.', '피터 잭슨', 134, 'N', 'Y', 'N', 'Y', '판타지', 0, '67TheLordoftheRingsTheFellowshipoftheRing.jpeg', 'www.youtube.com/embed/WZxJpHzEE');
+insert into tag values (317, '액션', 67);
+insert into tag values (318, '소설', 67);
+insert into actor values (317, '올랜도 블룸', 67);
+insert into actor values (318, '이안 맥켈런크루즈', 67);
+
+
+insert into movie values (68, '반지의 제왕: 두 개의 탑', '골룸은 프로도와 샘을 미나스 모르굴로 안내한다. 그곳에서 프로도와 샘은 아홉 나즈굴의 우두머리 앙그마르의 마술사왕이 오크 군단을 지휘하며 곤도르로 진군하기 시작하는 광경을 목격한다. 프로도와 샘은 골룸이 모르도르로 가는 비밀 지름길이라 했던, 절벽을 깎아 만든 계단길을 오르기 시작한다. 하지만 프로도는 골룸이 자신들을 죽이고 반지를 차지하기 위해 궁지에 몰아넣는다는 사실은 미처 눈치 채지 못한다. 한편 오크 대장 고스모그가 이끄는 미나스 모르굴의 선봉대는 오스길리아스를 야간 기습하여 점거하기에 이르고, 파라미르와 주둔군은 어쩔 수 없이 미나스 티리스로 후퇴하여 간달프의 지원을 통해 쫓아오는 나즈굴들의 손아귀에서 벗어나게 된다.', '피터 잭슨', 125, 'Y', 'Y', 'Y', 'Y', '판타지', 0, '68TheLordoftheRingsTheTwoTowers.jpeg', 'www.youtube.com/embed/Y4neBR0h39c');
+insert into tag values (319, '액션', 68);
+insert into tag values (320, '소설', 68);
+insert into tag values (321, '골룸', 68);
+insert into actor values (319, '올랜도 블룸즈', 68);
+insert into actor values (320, '이안 맥켈런', 68);
+
+insert into movie values (69, '반지의 제왕: 왕의 귀환', '호빗 스미골과 디골은 강에서 낚시를 즐기다 우연히 강 밑에서 절대반지를 줍는다. 스미골은 반지를 본 순간 현혹되어 반지를 갖기 위해 디골을 목졸라 죽인다. 하지만 디골을 죽인 죄 때문에 마을 사람들로부터 추방된 스미골은 안개산맥의 동굴 속으로 숨어들었고, 그곳에서 절대반지는 스미골의 육체와 정신을 서서히 망가뜨렸다. 긴 세월이 흘러 스미골은 결국 골룸이 되고 말았다.', '피터 잭슨', 124, 'N', 'Y', 'Y', 'N', '판타지', 0, '69TheLordoftheRingsTheReturnoftheKing.jpeg', 'www.youtube.com/embed/KOQSQaGgJxs');
+insert into tag values (322, '간달프', 69);
+insert into tag values (323, '액션', 69);
+insert into tag values (324, '골룸', 69);
+insert into actor values (321, '올랜도 블룸', 69);
+insert into actor values (322, '일라이저 우드', 69);
+
