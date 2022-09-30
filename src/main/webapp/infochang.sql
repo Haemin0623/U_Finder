f74f7ac4faa36630eabf5c62c459f726a5996f70
ChangRyul <ok_chang@naver.com>
2022-09-30 16:04:12 +0900

    db추가

---
+++ src/main/webapp/infochang.sql
@@ -0,0 +1,117 @@
+select * from movie;
+
+insert into movie values (40, '군함도', '1945년 일제강점기. 경성 반도호텔 악단장 ‘강옥’(황정민)과 그의 하나뿐인 딸 ‘소희’(김수안). 그리고 종로 일대를 주름잡던 주먹 ‘칠성’(소지섭), 일제 치하에서 온갖 고초를 겪어온 ‘말년’(이정현) 등 각기 다른 사연을 품은 조선인들이 일본에서 돈을 벌 수 있다는 말에 속아 군함도로 향한다. 하지만 그들이 함께 탄 배가 도착한 곳은 조선인들을 강제 징용해 광업소의 노동자로 착취하고 있던 ‘지옥섬’ 군함도였다.'
+, '류승완', 132, 'Y','N','Y','Y','액션', 0,'TheBattleshipIsland.jpg','https://www.youtube.com/embed/re7ZC-VNEOA');
+insert into tag values (200, '역사', 40);
+insert into tag values (201, '드라마', 40);
+insert into actor values (200, '황정민', 40);
+insert into actor values (201, '소지섭', 40);
+insert into actor values (202, '송중기', 40);
+insert into actor values (203, '이정현', 40);
+
+insert into movie values (41, '검사외전', '진실 앞에 무대뽀! 다혈질 검사, 살인 누명을 쓰고 감옥에 갇히다! 거친 수사 방식으로 유명한 다혈질 검사 ‘변재욱’(황정민). 취조 중이던 피의자가 변사체로 발견 되면서 살인 혐의로 체포된다. 꼼짝없이 살인 누명을 쓰게 된 변재욱은 결국 15년 형을 받고 수감된다.'
+, '이일형', 126, 'N','N','Y','N','범죄', 0,'AViolentProsecutor.jpg','https://www.youtube.com/embed/8-nu9mjVooc');
+insert into tag values (202, '코미디', 41);
+insert into actor values (204, '황정민', 41);
+insert into actor values (205, '강동원', 41);
+
+insert into movie values (42, '베테랑', '한번 필이 꽂히면 끝장을 볼때까지 파고드는 서도철, 껄렁함과 시원스러운 발차기가 몸에 베인 위장전문 홍일점 미스 봉, 육체파 왕형사와 신참내기 윤형사, 팀을 책임지고 있는 경력 20년차 오 팀장. 광수대 베테랑 형사인 그들은 중고차 사기범들을 멋지게 일망타진해내며 승진을 눈앞에 두고 한창 즐거워하고 있던 어느날, 도철은 아는 후배의 인맥으로 신진물산 셋째아들 조태오가 주최한 술자리에 초대받는다. 말끔하게 생긴 겉모습과는 달리 종종 이상한 행동을 자주하는 그를 보는 순간, 도철은 알수없는 수상함을 느낀다.'
+, '류승완', 123, 'Y','Y','N','N','범죄', 0,'Veteran.jpg','https://www.youtube.com/embed/24PZYgDMCh4');
+insert into tag values (203, '코미디', 42);
+insert into tag values (204, '액션', 42);
+insert into actor values (206, '황정민', 42);
+insert into actor values (207, '유아인', 42);
+insert into actor values (208, '유해진', 42);
+insert into actor values (209, '오달수', 42);
+
+insert into movie values (43, '신세계', '경찰청 수사기획과 강과장(최민식)은 국내 최대 범죄 조직인 골드문이 기업형 조직으로 그 세력이 점점 확장되자 신입 경찰 이자성(이정재)에게 잠입 수사를 명한다. 그리고 8년, 자성은 골드문의 2인자이자 그룹 실세인 정청(황정민)의 오른팔이 되기에 이른다.'
+, '박훈정', 134, 'Y','Y','N','Y','범죄', 0,'NewWorld.jpg','https://www.youtube.com/embed/F02upV3YeFI');
+insert into tag values (205, '드라마', 43);
+insert into tag values (206, '느와르', 43);
+insert into tag values (207, '피카레스크', 43);
+insert into actor values (210, '이정재', 43);
+insert into actor values (211, '최민식', 43);
+insert into actor values (212, '황정민', 43);
+insert into actor values (213, '송지효', 43);
+insert into actor values (214, '박성웅', 43);
+
+insert into movie values (44, '헌트', '망명을 신청한 북한 고위 관리를 통해 정보를 입수한 안기부 해외 팀 ‘박평호’(이정재)와 국내 팀 ‘김정도’(정우성)는 조직 내 숨어든 스파이 ‘동림’ 색출 작전을 시작한다.'
+, '이정재', 125, 'Y','N','Y','N','액션', 0,'HUNT.jpg','https://www.youtube.com/embed/CSOb8gia_mg');
+insert into tag values (208, '스릴러', 44);
+insert into tag values (209, '드라마', 44);
+insert into tag values (210, '첩보', 44);
+insert into tag values (211, '느와르', 44);
+insert into tag values (212, '시대극', 44);
+insert into actor values (215, '이정재', 44);
+insert into actor values (216, '정우성', 44);
+insert into actor values (217, '전혜진', 44);
+insert into actor values (218, '허성태', 44);
+insert into actor values (219, '고윤정', 44);
+
+insert into movie values (45, '공작', '북으로 간 스파이, 암호명 흑금성 1993년, 북한 핵 개발을 둘러싸고 한반도의 위기가 고조된다. 정보사 소령 출신으로 안기부에 스카우트된 박석영(황정민)은 ‘흑금성’이라는 암호명으로 북핵의 실체를 캐기 위해 북의 고위층 내부로 잠입하라는 지령을 받는다.'
+, '윤종빈', 137, 'N','N','Y','Y','드라마', 0,'TheSpyGoneNorth.jpg','https://www.youtube.com/embed/XyEJxOfKaCw');
+insert into tag values (213, '첩보', 45);
+insert into tag values (214, '시대극', 45);
+insert into actor values (220, '황정민', 45);
+insert into actor values (221, '이성민', 45);
+insert into actor values (222, '조진웅', 45);
+insert into actor values (223, '주지훈', 45);
+
+insert into movie values (46, '암살', '안옥윤, 속사포, 황덕삼은 김원봉의 제안과 상하이 대한민국 임시정부 경무국 대장 염석진의 주도로 매국노 강인국과 조선 주둔군 사령관인 일본 육군 소장 카와구치 마모루를 암살하기 위한 작전을 전개한다.'
+, '최동훈', 132, 'Y','N','Y','Y','드라마', 0,'Assassination.jpg','https://www.youtube.com/embed/YzleZY-TahM');
+insert into tag values (215, '시대극', 46);
+insert into actor values (224, '전지현', 46);
+insert into actor values (225, '이정재', 46);
+insert into actor values (226, '하정우', 46);
+insert into actor values (227, '오달수', 46);
+insert into actor values (228, '조진웅', 46);
+insert into actor values (229, '이경영', 46);
+insert into actor values (230, '최덕문', 46);
+
+insert into movie values (47, '도둑들', '한팀으로 활동 중인 한국의 도둑 뽀빠이와 예니콜, 씹던껌, 잠파노. 미술관을 터는데 멋지게 성공한 이들은 뽀빠이의 과거 파트너였던 마카오 박이 제안한 홍콩에서의 새로운 계획을 듣게 된다. 여기에 마카오 박이 초대하지 않은 손님, 감옥에서 막 출소한 금고털이 팹시가 합류하고 5명은 각자 인생 최고의 반전을 꿈꾸며 홍콩으로 향한다.'
+, '최동훈', 132, 'Y','Y','Y','N','범죄', 0,'TheThieves.jpg','https://www.youtube.com/embed/seGzMn5qu2M');
+insert into tag values (216, '액션', 47);
+insert into tag values (217, '드라마', 47);
+insert into actor values (231, '김윤석', 47);
+insert into actor values (232, '김혜수', 47);
+insert into actor values (233, '이정재', 47);
+insert into actor values (234, '전지현', 47);
+insert into actor values (235, '임달화', 47);
+insert into actor values (236, '김해숙', 47);
+insert into actor values (237, '오달수', 47);
+insert into actor values (238, '김수현', 47);
+
+insert into movie values (48, '신과함께-죄와벌', '저승 법에 의하면, 모든 인간은 사후 49일 동안 7번의 재판을 거쳐야만 한다. 살인, 나태, 거짓, 불의, 배신, 폭력, 천륜 7개의 지옥에서 7번의 재판을 무사히 통과한 망자만이 환생하여 새로운 삶을 시작할 수 있다."김자홍 씨께선, 오늘 예정대로 무사히 사망하셨습니다." 화재 사고 현장에서 여자아이를 구하고 죽음을 맞이한 소방관 자홍, 그의 앞에 저승차사 해원맥과 덕춘이 나타난다.'
+, '김용화', 139, 'Y','N','N','Y','액션', 0,'AlongwiththeGodsTheTwoWorlds.jpg','https://www.youtube.com/embed/SC_JhOyvCXw');
+insert into tag values (218, '판타지', 48);
+insert into tag values (219, '드라마', 48);
+insert into tag values (220, '어드벤처', 48);
+insert into tag values (221, '디스토피아', 48);
+insert into actor values (239, '하정우', 48);
+insert into actor values (240, '차태현', 48);
+insert into actor values (241, '주지훈', 48);
+insert into actor values (242, '김향기', 48);
+
+insert into movie values (49, '신과함께-인과연', '천 년 동안 48명의 망자를 환생시킨 저승 삼차사, 한 명만 더 환생시키면 그들도 새로운 삶을 얻을 수 있다. 하지만 강림(하정우)은 원귀였던 수홍(김동욱)을 자신들의 마지막 귀인으로 정하는 이해할 수 없는 선택을 한다. 저승법상 원귀는 소멸되어야 마땅하나 염라대왕(이정재)은 저승 삼차사에게 새로운 조건을 내걸며 강림의 제안을 수락한다. 염라의 조건은 성주신(마동석)이 버티고 있어 저승 차사들이 가는 족족 실패하는 허춘삼 노인을 수홍의 재판이 끝나기 전까지 저승으로 데려오는 것. '
+, '김용화', 132, 'Y','Y','Y','Y','액션', 0,'AlongwiththeGodsTheLast49Days.jpg','https://www.youtube.com/embed/53f99njhgB8');
+insert into tag values (222, '판타지', 49);
+insert into tag values (223, '드라마', 49);
+insert into tag values (224, '어드벤처', 49);
+insert into actor values (243, '하정우', 49);
+insert into actor values (244, '주지훈', 49);
+insert into actor values (245, '김향기', 49);
+insert into actor values (246, '마동석', 49);
+insert into actor values (247, '김동욱', 49);
+
+insert into movie values (50, '밀정', '1920년대 일제강점기. 조선인 출신 일본경찰 이정출(송강호)은 무장독립운동 단체 의열단의 뒤를 캐라는 특명으로 의열단의 리더 김우진(공유)에게 접근하고, 한 시대의 양 극단에 서 있는 두 사람은 서로의 정체와 의도를 알면서도 속내를 감춘 채 가까워진다. 출처를 알 수 없는 정보가 쌍방간에 새어나가고 누가 밀정인지 알 수 없는 가운데,의열단은 일제의 주요 시설을 파괴할 폭탄을 경성으로 들여오기 위해, 그리고 일본제국 경찰은 그들을 쫓아 모두 상해에 모인다.'
+, '김지운', 140, 'N','Y','N','Y','액션', 0,'TheAgeofShadows.jpg','https://www.youtube.com/embed/bVXhxAdytfI');
+
+insert into tag values (225, '스릴러', 50);
+insert into tag values (226, '첩보물', 50);
+insert into actor values (248, '송강호', 50);
+insert into actor values (249, '공유', 50);
+insert into actor values (250, '한지민', 50);
+insert into actor values (251, '츠루미신고', 50);
+insert into actor values (252, '엄태구', 50);
+insert into actor values (253, '신성록', 50);
+
